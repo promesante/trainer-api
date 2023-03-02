b8cb6b661cadc37d465f2633b3edc7881ac6c913
Luis M. Rosso <lrosso@santandertecnologia.com.ar>
2023-03-02 17:19:43 -0300

    implementing endpoints

---
+++ src/main/java/com/campgladiator/trainer/Trainer.java
@@ -0,0 +1,29 @@
+package com.campgladiator.trainer;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class Trainer {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String email;
+    private String phone;
+    private String firstName;
+    private String lastName;
+}
